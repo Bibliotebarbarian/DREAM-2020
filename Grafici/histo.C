0c6afb19d4d1ce89e8f8085affb822a25688a2a4
Great-Pirate-King <81078254+Great-Pirate-King@users.noreply.github.com>
2021-05-31 11:21:13 +0200

    Update histo.C

--- Grafici/histo.C
+++ Grafici/histo.C
@@ -10,7 +10,7 @@
 
 #include <string>
 #include <cmath>
-void histo(TString file){
+void original(TString file){
 	
 	
 
@@ -57,17 +57,18 @@ void histo(TString file){
 	Double_t minShift[4];
 	Double_t maxShift[4];
 	
+	Int_t isGoodEvent=0;
 	
 	t1->SetBranchAddress("signalStart", &signalStart);
 	t1->SetBranchAddress("shiftedStart", &shiftedStart);
 	t1->SetBranchAddress("chargeValue",&chargeValue);
-	t1->SetBranchAddress("minCharge",&minCharge);
-	t1->SetBranchAddress("maxCharge",&maxCharge);
-	t1->SetBranchAddress("minStart",&minStart);
-	t1->SetBranchAddress("maxStart",&maxStart);
-	t1->SetBranchAddress("minShift",&minShift);
-	t1->SetBranchAddress("maxShift",&maxShift);
-	
+	//t1->SetBranchAddress("minCharge",&minCharge);       //non servono perchè uso range che esclude eventi oltre 3 sigma
+	//t1->SetBranchAddress("maxCharge",&maxCharge);
+	//t1->SetBranchAddress("minStart",&minStart);
+	//t1->SetBranchAddress("maxStart",&maxStart);
+	//t1->SetBranchAddress("minShift",&minShift);
+	//t1->SetBranchAddress("maxShift",&maxShift);
+	t1->SetBranchAddress("isGoodEvent",&isGoodEvent);
 	
 	cout << "number of entries : " << nentries << endl;	//nentries sono gli eventi
 	//nsamples sono i bin di ogni evento
@@ -80,22 +81,33 @@ void histo(TString file){
 		t1->GetEntry(entry);
 		//nsample-=1;
 		//cout<<chargeValue[0]<<endl;
-		
-		for(int j=0; j<4; j++){
-			ChargeHist[j]->Fill(chargeValue[j]);
-			StartHist[j]->Fill(signalStart[j]);
-			ShTime[j]->Fill(shiftedStart[j]);
+		if(isGoodEvent==1){
+		      for(int j=0; j<4; j++){
+			      ChargeHist[j]->Fill(chargeValue[j]);
+			      StartHist[j]->Fill(signalStart[j]);
+			      ShTime[j]->Fill(shiftedStart[j]);
+		      }
 		}
 	}
 	
+	for(int i=0; i<4; i++){
 	
+	    minCharge[i]=ChargeHist[i]->GetXaxis()->GetBinCenter(ChargeHist[i]->FindFirstBinAbove());
+	    maxCharge[i]=ChargeHist[i]->GetXaxis()->GetBinCenter(ChargeHist[i]->FindLastBinAbove());
+	    
+	    minStart[i]=StartHist[i]->GetXaxis()->GetBinCenter(StartHist[i]->FindFirstBinAbove());
+	    maxStart[i]=StartHist[i]->GetXaxis()->GetBinCenter(StartHist[i]->FindLastBinAbove());
+	    
+	    minShift[i]=ShTime[i]->GetXaxis()->GetBinCenter(ShTime[i]->FindFirstBinAbove());
+	    maxShift[i]=ShTime[i]->GetXaxis()->GetBinCenter(ShTime[i]->FindLastBinAbove());
 	
+	}
 	
 	for(int j=0; j<4; j++){
 		cout << minCharge[j] << " " << maxCharge[j] << " " << minStart[j] << " " << maxStart[j] << " " << minShift[j] << " " << maxShift[j] << endl;
-		ChargeHist[j]->SetBins(50, minCharge[j], maxCharge[j]);
-		StartHist[j]->SetBins(50, minStart[j], maxStart[j]);
-		ShTime[j]->SetBins(50, minShift[j], maxShift[j]);
+		ChargeHist[j]->SetBins(100, minCharge[j], maxCharge[j]);
+		StartHist[j]->SetBins(100, minStart[j], maxStart[j]);
+		ShTime[j]->SetBins(100, minShift[j], maxShift[j]);
 		
 		ChargeHist[j]->Rebin();
 		StartHist[j]->Rebin();
